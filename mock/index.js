95432381f860a78bd097873bae8ceabe6a096bec
T179012-黃昱超 <wilson.huang@kerrytj.com>
2020-12-16 09:09:31 +0800

    權限範本

--- mock/index.js
+++ mock/index.js
@@ -4,11 +4,13 @@ const { param2Obj } = require('./utils')
 const user = require('./user')
 const table = require('./table')
 const organization = require('./organization')
+const permissionTemplate = require('./permission-template')
 
 const mocks = [
   ...user,
   ...table,
-  ...organization
+  ...organization,
+  ...permissionTemplate
 ]
 
 // for front mock
